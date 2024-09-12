f8553758068665972ac2a69f12ac1b318c00cbec
davidliu <davidliu@deviange.net>
2024-09-12 15:35:27 +0900

    rememberTranscriptions hooks

--- livekit-compose-components/src/main/java/io/livekit/android/compose/flow/RoomFlows.kt
+++ livekit-compose-components/src/main/java/io/livekit/android/compose/flow/RoomFlows.kt
@@ -19,9 +19,16 @@ package io.livekit.android.compose.flow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import io.livekit.android.events.EventListenable
+import io.livekit.android.events.ParticipantEvent
 import io.livekit.android.events.RoomEvent
+import io.livekit.android.events.TrackEvent
+import io.livekit.android.events.TrackPublicationEvent
 import io.livekit.android.events.collect
 import io.livekit.android.room.Room
+import io.livekit.android.room.participant.Participant
+import io.livekit.android.room.track.Track
+import io.livekit.android.room.track.TrackPublication
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 
@@ -37,12 +44,64 @@ import kotlinx.coroutines.flow.MutableSharedFlow
  */
 @Composable
 inline fun <reified T : RoomEvent> rememberEventSelector(room: Room): Flow<T> {
-    val flow = remember(room) {
+    return rememberEventSelector<T, RoomEvent>(eventListenable = room.events)
+}
+
+/**
+ * A utility method to obtain a flow for specific room events.
+ *
+ * Pass in [ParticipantEvent] as the type to receive all room events.
+ *
+ * ```
+ * // Receive only participant connected events.
+ * val eventFlow = rememberEventSelector<ParticipantEvent.SpeakingChanged>(participant)
+ * ```
+ */
+@Composable
+inline fun <reified T : ParticipantEvent> rememberEventSelector(participant: Participant): Flow<T> {
+    return rememberEventSelector<T, ParticipantEvent>(eventListenable = participant.events)
+}
+
+/**
+ * A utility method to obtain a flow for specific room events.
+ *
+ * Pass in [TrackPublicationEvent] as the type to receive all publication events.
+ *
+ * ```
+ * // Receive only participant connected events.
+ * val eventFlow = rememberEventSelector<TrackPublicationEvent.TranscriptionReceived>(publication)
+ * ```
+ */
+@Composable
+inline fun <reified T : TrackPublicationEvent> rememberEventSelector(publication: TrackPublication): Flow<T> {
+    return rememberEventSelector<T, TrackPublicationEvent>(eventListenable = publication.events)
+}
+
+/**
+ * A utility method to obtain a flow for specific track events.
+ *
+ * Pass in [TrackEvent] as the type to receive all track events.
+ *
+ * ```
+ * // Receive only stream state changed events.
+ * val eventFlow = rememberEventSelector<TrackEvent.StreamStateChanged>(track)
+ * ```
+ */
+@Composable
+inline fun <reified T : TrackEvent> rememberEventSelector(track: Track): Flow<T> {
+    return rememberEventSelector<T, TrackEvent>(eventListenable = track.events)
+}
+
+@Composable
+inline fun <reified T, U> rememberEventSelector(eventListenable: EventListenable<U>): Flow<T>
+        where T : U {
+
+    val flow = remember(eventListenable) {
         MutableSharedFlow<T>(extraBufferCapacity = 100)
     }
 
-    LaunchedEffect(room) {
-        room.events.collect {
+    LaunchedEffect(eventListenable) {
+        eventListenable.collect {
             if (it is T) {
                 flow.emit(it)
             }
--- livekit-compose-components/src/main/java/io/livekit/android/compose/state/transcriptions/RememberTranscriptions.kt
+++ livekit-compose-components/src/main/java/io/livekit/android/compose/state/transcriptions/RememberTranscriptions.kt
@@ -2,45 +2,78 @@ package io.livekit.android.compose.state.transcriptions
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
 import io.livekit.android.annotations.Beta
 import io.livekit.android.compose.flow.rememberEventSelector
+import io.livekit.android.compose.local.requireParticipant
+import io.livekit.android.compose.local.requireRoom
 import io.livekit.android.compose.types.TrackReference
+import io.livekit.android.events.ParticipantEvent
+import io.livekit.android.events.RoomEvent
 import io.livekit.android.events.TrackPublicationEvent
 import io.livekit.android.room.Room
+import io.livekit.android.room.participant.Participant
 import io.livekit.android.room.types.TranscriptionSegment
+import io.livekit.android.room.types.mergeNewSegments
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 /**
  * Collect all the transcriptions for the room.
  */
 @Beta
 @Composable
-fun rememberRoomTranscriptions(room: Room) {
+fun rememberTranscriptions(passedRoom: Room? = null): List<TranscriptionSegment> {
+    val room = requireRoom(passedRoom)
+    val events = rememberEventSelector<RoomEvent.TranscriptionReceived>(room)
+        .map { it.transcriptionSegments }
 
+    return rememberTranscriptionsImpl(transcriptionsFlow = events)
 }
 
 /**
- * Collect all the transcriptions for a track.
+ * Collect all the transcriptions for a track reference.
  */
 @Beta
 @Composable
-fun rememberTrackTranscriptions(trackReference: TrackReference) {
-    val segments = remember { mutableStateListOf<TranscriptionSegment>() }
+fun rememberTranscriptions(trackReference: TrackReference): List<TranscriptionSegment> {
+    val publication = trackReference.publication ?: return emptyList()
+    val events = rememberEventSelector<TrackPublicationEvent.TranscriptionReceived>(publication)
+        .map { it.transcriptions }
 
-    if(trackReference.publication != null) {
-        val events = rememberEventSelector<TrackPublicationEvent.TranscriptionReceived>(trackReference.publication)
-
-        LaunchedEffect(events) {
-            segments.clear()
-            events.collect {
-
-            }
-        }
-    }
+    return rememberTranscriptionsImpl(transcriptionsFlow = events)
 }
 
-data class ReceivedTranscriptionSegment(
-    val segment: TranscriptionSegment,
-    val timestamp: Long,
-)
+/**
+ * Collect all the transcriptions for a participant.
+ */
+@Beta
+@Composable
+fun rememberTranscriptions(passedParticipant: Participant? = null): List<TranscriptionSegment> {
+    val participant = requireParticipant(passedParticipant)
+    val events = rememberEventSelector<ParticipantEvent.TranscriptionReceived>(participant)
+        .map { it.transcriptions }
+
+    return rememberTranscriptionsImpl(transcriptionsFlow = events)
+}
+
+
+@Composable
+internal fun rememberTranscriptionsImpl(transcriptionsFlow: Flow<List<TranscriptionSegment>>): List<TranscriptionSegment> {
+    val segments = remember { mutableStateMapOf<String, TranscriptionSegment>() }
+    val orderedSegments = remember {
+        derivedStateOf {
+            segments.values.sortedBy { segment -> segment.firstReceivedTime }
+        }
+    }
+    LaunchedEffect(transcriptionsFlow) {
+        segments.clear()
+        transcriptionsFlow.collect {
+            segments.mergeNewSegments(it)
+        }
+    }
+
+    return orderedSegments.value
+}
