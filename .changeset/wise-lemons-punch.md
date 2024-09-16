99160e7eb27bf699fbfca35939425fe8b1b87e23
davidliu <davidliu@deviange.net>
2024-09-16 13:34:47 +0900

    Add changesets for releasing (#26)
    * Add changesets for releasing
    
    * note in readme to bring in livekit sdk separately
    
    * fix update version script

---
+++ .changeset/wise-lemons-punch.md
@@ -0,0 +1,5 @@
+---
+"components-android": minor
+---
+
+Add rememberEventSelector implementations for other types of events (e.g. ParticipantEvent, TrackEvent)
---
+++ ci/update_android_gradle_version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+set -x
+
+PACKAGE_VERSION=$(cat ./package.json | jq -r '.version')
+>&2 echo "updating gradle version name to $PACKAGE_VERSION"
+
+SNAPSHOT_VERSION=$(./ci/increment_semver.sh -p $PACKAGE_VERSION)"-SNAPSHOT"
+>&2 echo "next snapshot version to $SNAPSHOT_VERSION"
+
+# sed command works only on linux based systems as macOS version expects a backup file passed additionally
+
+if [ "$(uname)" == "Darwin" ]; then
+  ARGS=('')
+else
+  ARGS=()
+fi
+
+sed -i "${ARGS[@]}" -e "/VERSION_NAME=/ s/=.*/=$PACKAGE_VERSION/" ./gradle.properties
+sed -i "${ARGS[@]}" -e '/livekit-android-compose-components:.*[0-9]"$/ s/livekit-android-compose-components:.*/livekit-android-compose-components:'"$PACKAGE_VERSION'/" ./README.md
+sed -i "${ARGS[@]}" -e '/SNAPSHOT/ s/".*"/"'"io.livekit:livekit-android-compose-components:$SNAPSHOT_VERSION"'"/' ./README.md
