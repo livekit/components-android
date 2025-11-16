be10562b71be4724b7e224707b784ea6fe12882a
davidliu <davidliu@deviange.net>
2025-11-16 20:30:46 +0900

    rememberSessionMessages implementation

---
+++ livekit-compose-components/src/main/java/io/livekit/android/compose/state/RememberSessionMessages.kt
@@ -0,0 +1,152 @@
+package io.livekit.android.compose.state
+
+import android.os.SystemClock
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import io.livekit.android.annotations.Beta
+import io.livekit.android.compose.chat.rememberChat
+import io.livekit.android.compose.local.requireSession
+import io.livekit.android.compose.state.transcriptions.rememberTranscriptions
+import io.livekit.android.compose.types.ReceivedAgentTranscriptionMessage
+import io.livekit.android.compose.types.ReceivedChatMessage
+import io.livekit.android.compose.types.ReceivedMessage
+import io.livekit.android.compose.types.ReceivedUserTranscriptionMessage
+import io.livekit.android.room.datastream.StreamTextOptions
+import kotlinx.coroutines.launch
+
+interface SessionMessages {
+    /**
+     * The log of all messages sent and received.
+     */
+    val messages: List<ReceivedMessage>
+
+    /**
+     * A hot flow emitting a [ReceivedMessage] for each individual message sent and received.
+     */
+    //TODO val messagesFlow: Flow<ReceivedMessage>
+
+    val isSending: Boolean
+
+    suspend fun send(message: String, options: StreamTextOptions = StreamTextOptions()): Result<ReceivedChatMessage>
+}
+
+internal class SessionMessagesImpl(
+    private val messagesState: State<List<ReceivedMessage>>,
+    val sendImpl: suspend (message: String, options: StreamTextOptions) -> Result<ReceivedChatMessage>
+) : SessionMessages {
+    override val messages
+        get() = messagesState.value
+    override var isSending by mutableStateOf(false) // TOD
+        internal set
+
+    override suspend fun send(
+        message: String,
+        options: StreamTextOptions,
+    ): Result<ReceivedChatMessage> {
+        return sendImpl(message, options)
+    }
+}
+
+@Beta
+@Composable
+fun rememberSessionMessages(session: Session? = null): SessionMessages {
+    val session = requireSession(session)
+    val room = session.room
+    val agent = rememberAgent(session)
+
+    val transcriptions by rememberTranscriptions(room)
+    val chat = rememberChat(room = room)
+    val transcriptionMessages by remember(room) {
+        derivedStateOf {
+            transcriptions.map { transcription ->
+                when (transcription.participantIdentity) {
+                    room.localParticipant.identity -> {
+                        // User transcription
+                        ReceivedUserTranscriptionMessage(
+                            id = transcription.streamInfo.id,
+                            message = transcription.text,
+                            timestamp = transcription.streamInfo.timestampMs,
+                            fromParticipant = room.localParticipant,
+                        )
+                    }
+
+                    agent.agentParticipant?.identity,
+                    agent.workerParticipant?.identity -> {
+                        ReceivedAgentTranscriptionMessage(
+                            id = transcription.streamInfo.id,
+                            message = transcription.text,
+                            timestamp = transcription.streamInfo.timestampMs,
+                            fromParticipant = if (agent.agentParticipant?.identity == transcription.participantIdentity) {
+                                agent.agentParticipant!!
+                            } else {
+                                agent.workerParticipant!!
+                            },
+                        )
+                    }
+
+                    else -> {
+                        // FIXME: what should happen if an associated participant is not found?
+                        //
+                        // For now, just assume it is an agent transcription, since maybe it is from an agent
+                        // which disconnected from the room or something like that.
+                        ReceivedAgentTranscriptionMessage(
+                            id = transcription.streamInfo.id,
+                            message = transcription.text,
+                            timestamp = transcription.streamInfo.timestampMs,
+                            fromParticipant = room.remoteParticipants.values.firstOrNull { p -> p.identity == transcription.participantIdentity }
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    val receivedMessagesTimeMap = remember {
+        mutableStateMapOf<String, Long>()
+    }
+    val receivedMessages = remember {
+        derivedStateOf {
+            (transcriptionMessages.plus(elements = chat.messages.value))
+                .sortedBy { receivedMessagesTimeMap[it.id] }
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        launch {
+            snapshotFlow { transcriptionMessages }
+                .collect { messages ->
+                    for (message in messages) {
+                        val original = receivedMessagesTimeMap[message.id]
+                        if (original == null) {
+                            receivedMessagesTimeMap[message.id] = SystemClock.elapsedRealtime()
+                        }
+                    }
+                }
+        }
+        launch {
+            chat.messagesFlow
+                .collect { message -> receivedMessagesTimeMap[message.id] = SystemClock.elapsedRealtime() }
+        }
+    }
+
+
+    val sessionMessages = remember(chat) {
+        SessionMessagesImpl(
+            messagesState = receivedMessages,
+            { message, options ->
+                chat.send(message, options)
+            }
+        )
+    }
+
+    return sessionMessages
+}
+
