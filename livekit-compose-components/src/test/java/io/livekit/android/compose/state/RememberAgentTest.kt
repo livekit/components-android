fac0d219ad8fe8309019d3042c4dc3eacbc52ee5
davidliu <davidliu@deviange.net>
2025-11-17 01:48:10 +0900

    agent state testing

--- livekit-compose-components/src/test/java/io/livekit/android/compose/state/RememberAgentTest.kt
+++ livekit-compose-components/src/test/java/io/livekit/android/compose/state/RememberAgentTest.kt
@@ -29,6 +29,7 @@ import kotlinx.coroutines.launch
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import kotlin.time.Duration.Companion.milliseconds
 
 @OptIn(Beta::class)
 class RememberAgentTest : MockE2ETest() {
@@ -68,13 +69,57 @@ class RememberAgentTest : MockE2ETest() {
     }
 
     @Test
+    fun agentStateWithoutPreconnect() = runTest {
+        val job = launch {
+            moleculeFlow(RecompositionMode.Immediate) {
+                val session = rememberSession(
+                    tokenSource = TokenSource.fromLiteral(TestData.EXAMPLE_URL, "token"),
+                    options = SessionOptions(
+                        room = room,
+                    )
+                )
+
+                val agent = rememberAgent(session)
+
+                LaunchedEffect(Unit) {
+                    val startResult = session.start(
+                        options = SessionConnectOptions(
+                            tracks = SessionConnectTrackOptions(
+                                microphoneEnabled = false,
+                                usePreconnectBuffer = false,
+                            )
+                        )
+                    )
+                    assertTrue(startResult.isSuccess)
+
+                    val agentJoin = TestData.AGENT_JOIN
+                    simulateMessageFromServer(agentJoin)
+
+                    session.end()
+                }
+                agent.agentState
+            }
+                .distinctUntilChanged()
+                .composeTest {
+                    assertEquals(AgentState.DISCONNECTED, awaitItem())
+                    assertEquals(AgentState.CONNECTING, awaitItem())
+                    assertEquals(AgentState.LISTENING, awaitItem())
+                    assertEquals(AgentState.DISCONNECTED, awaitItem())
+                }
+        }
+
+        sessionConnect()
+
+        job.join()
+    }
+    @Test
     fun agentStateWithPreconnect() = runTest {
         val job = launch {
             moleculeFlow(RecompositionMode.Immediate) {
                 val session = rememberSession(
                     tokenSource = TokenSource.fromLiteral(TestData.EXAMPLE_URL, "token"),
                     options = SessionOptions(
-                        room = room
+                        room = room,
                     )
                 )
 
@@ -93,8 +138,85 @@ class RememberAgentTest : MockE2ETest() {
                 .distinctUntilChanged()
                 .composeTest {
                     assertEquals(AgentState.DISCONNECTED, awaitItem())
+                    assertEquals(AgentState.CONNECTING, awaitItem())
                     assertEquals(AgentState.LISTENING, awaitItem())
                     assertEquals(AgentState.DISCONNECTED, awaitItem())
+                }
+        }
+
+        sessionConnect()
+
+        job.join()
+    }
+
+
+    @Test
+    fun agentTimeoutWithPreconnect() = runTest {
+        val job = launch {
+            moleculeFlow(RecompositionMode.Immediate) {
+                val session = rememberSession(
+                    tokenSource = TokenSource.fromLiteral(TestData.EXAMPLE_URL, "token"),
+                    options = SessionOptions(
+                        room = room,
+                        agentConnectTimeout = 100.milliseconds
+                    )
+                )
+
+                val agent = rememberAgent(session)
+
+                LaunchedEffect(Unit) {
+                    val startResult = session.start()
+                    assertTrue(startResult.isSuccess)
+                }
+                agent.agentState
+            }
+                .distinctUntilChanged()
+                .composeTest {
+                    assertEquals(AgentState.DISCONNECTED, awaitItem())
+                    assertEquals(AgentState.CONNECTING, awaitItem())
+                    assertEquals(AgentState.LISTENING, awaitItem())
+                    assertEquals(AgentState.FAILED, awaitItem())
+                }
+        }
+
+        sessionConnect()
+
+        job.join()
+    }
+
+
+    @Test
+    fun agentTimeoutWithoutPreconnect() = runTest {
+        val job = launch {
+            moleculeFlow(RecompositionMode.Immediate) {
+                val session = rememberSession(
+                    tokenSource = TokenSource.fromLiteral(TestData.EXAMPLE_URL, "token"),
+                    options = SessionOptions(
+                        room = room,
+                        agentConnectTimeout = 100.milliseconds
+                    )
+                )
+
+                val agent = rememberAgent(session)
+
+                LaunchedEffect(Unit) {
+                    val startResult = session.start(
+                        options = SessionConnectOptions(
+                            tracks = SessionConnectTrackOptions(
+                                microphoneEnabled = false,
+                                usePreconnectBuffer = false,
+                            )
+                        )
+                    )
+                    assertTrue(startResult.isSuccess)
+                }
+                agent.agentState
+            }
+                .distinctUntilChanged()
+                .composeTest {
+                    assertEquals(AgentState.DISCONNECTED, awaitItem())
+                    assertEquals(AgentState.CONNECTING, awaitItem())
+                    assertEquals(AgentState.FAILED, awaitItem())
                 }
         }
 
--- livekit-compose-components/src/test/java/io/livekit/android/compose/test/util/TurbineExt.kt
+++ livekit-compose-components/src/test/java/io/livekit/android/compose/test/util/TurbineExt.kt
@@ -17,17 +17,16 @@
 package io.livekit.android.compose.test.util
 
 import app.cash.turbine.TurbineTestContext
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.yield
 import kotlin.time.Duration
 import app.cash.turbine.test as turbineTest
 
 /**
  * Due to the use of [kotlinx.coroutines.test.UnconfinedTestDispatcher],
- * Turbine fails to validate unconsumed events. This adds a small delay
- * at the end of the validation block to release the coroutine to finish
- * any extra work.
+ * Turbine fails to validate unconsumed events. This yields at the end of
+ * the validation block to release the coroutine to finish any extra work.
  */
 suspend fun <T> Flow<T>.composeTest(
     distinctUntilChanged: Boolean = true,
@@ -45,7 +44,7 @@ suspend fun <T> Flow<T>.composeTest(
         name,
         {
             validate()
-            delay(1)
+            yield()
         }
     )
 }
